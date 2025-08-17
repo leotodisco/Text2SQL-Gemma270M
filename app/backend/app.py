8f8828638137e47d071261349db2f31d76ff7b58
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2025-08-17 21:57:40 +0200

    update SFT code

--- app/backend/app.py
+++ app/backend/app.py
@@ -7,6 +7,7 @@ load_dotenv(".env")
 token=os.getenv("HF_TOKEN")
 
 tokenizer = AutoTokenizer.from_pretrained("leotod/gemma-270m-Text2SQL-Fine-tuned", token=token)
+print(tokenizer.chat_template)
 model = AutoModelForCausalLM.from_pretrained("leotod/gemma-270m-Text2SQL-Fine-tuned", token=token)
 stop_token_ids = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<end_of_turn>")]
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
@@ -22,17 +23,19 @@ user_prompt = """Given the <USER_QUERY> and the <SCHEMA>, generate the correspon
 </USER_QUERY>
 """
 
+system_message="You are a helpful assistant skilled in translating natural language into SQL Queries given the Table DDL. You will respond only with the correct MySQL Query."
+
 def create_conversation(sql_prompt: str, sql_context: str = ""):
   return {
     "messages": [
-      # {"role": "system", "content": system_message},
+      {"role": "system", "content": system_message},
       {"role": "user", "content": user_prompt.format(question=sql_prompt, context=sql_context)},
     ]
   }
 
 def predict(conversation: list[dict]):
     prompt = pipe.tokenizer.apply_chat_template(conversation["messages"], tokenize=False, add_generation_prompt=True)
-    outputs = pipe(prompt, max_new_tokens=256, do_sample=False, temperature=0.1, top_k=50, top_p=0.1, eos_token_id=stop_token_ids, disable_compile=True)
+    outputs = pipe(prompt, max_new_tokens=40, do_sample=False, temperature=0.0, top_k=50, top_p=0.1, eos_token_id=stop_token_ids, disable_compile=True)
     
     return outputs[0]['generated_text'][len(prompt):].strip()
 
